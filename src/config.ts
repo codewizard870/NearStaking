66908b51d8ef307679542efce1eff5a3db82a37c
alenzer <alenzer0902@gmail.com>
2022-06-01 05:07:34 -0400

    change contract

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_NAME = 'pool.alenzertest.testnet'; /* TODO: fill this in! */
+export const CONTRACT_NAME = 'staking.alenzertest.testnet'; /* TODO: fill this in! */
 
 export function getConfig (env: any) {
   switch (env) {
